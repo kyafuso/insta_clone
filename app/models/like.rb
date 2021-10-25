0cc2911e0fae4b7b97d799b73514ebadfd037e3f
kyafuso <kazuya.yafuso.0825@gmail.com>
2021-10-25 12:24:09 +0900

    基本的なアクティビティの機能実装

--- app/models/like.rb
+++ app/models/like.rb
@@ -24,4 +24,12 @@ class Like < ApplicationRecord
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: :post_id }
+
+  after_create_commit :create_activities
+
+  private
+
+  def create_activities
+    Activity.create(subject: self, user: post.user, action_type: :liked_to_own_post)
+  end
 end
