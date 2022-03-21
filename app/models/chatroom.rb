279d7445b30e12a731b235f2847ca4622da31faf
kyafuso <kazuya.yafuso.0825@gmail.com>
2022-03-21 10:55:08 +0900

    [Add]be rails g model chatroom

---
+++ app/models/chatroom.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: chatrooms
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Chatroom < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_102957) do
+ActiveRecord::Schema.define(version: 2022_03_21_014907) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "subject_type"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2021_11_11_102957) do
     t.datetime "updated_at", null: false
     t.index ["subject_type", "subject_id"], name: "index_activities_on_subject_type_and_subject_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "chatrooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
