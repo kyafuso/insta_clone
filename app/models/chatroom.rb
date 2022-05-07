1b2a2783e71f3194c4cbcb5199798b82deea2fe3
kyafuso <kazuya.yafuso.0825@gmail.com>
2022-05-07 15:51:07 +0900

    [fix]be rubocop -a

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -13,8 +13,8 @@ class Chatroom < ApplicationRecord
   has_many :users, through: :chatroom_users
   has_many :messages, dependent: :destroy
 
-  scope :public_channels, ->{ where(direct_message: false) }
-  scope :direct_messages, ->{ where(direct_message: true) }
+  scope :public_channels, -> { where(direct_message: false) }
+  scope :direct_messages, -> { where(direct_message: true) }
 
   def self.chatroom_for_users(users, direct_message: false)
     user_ids = users.map(&:id).sort
