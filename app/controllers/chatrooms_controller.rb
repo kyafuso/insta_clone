d1f1055f9b77dfc9054bd995d5a0de50f386b380
kyafuso <kazuya.yafuso.0825@gmail.com>
2022-05-08 18:49:52 +0900

    [fix]N+1問題解消

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,7 +2,7 @@ class ChatroomsController < ApplicationController
   before_action :require_login, only: %i[index show]
 
   def index
-    @chatrooms = current_user.chatrooms.page(params[:page]).order(created_at: :desc)
+    @chatrooms = current_user.chatrooms.includes(:users, :messages).page(params[:page]).order(created_at: :desc)
   end
 
   def create
@@ -14,6 +14,6 @@ class ChatroomsController < ApplicationController
   end
 
   def show
-    @chatroom = current_user.chatrooms.find(params[:id])
+    @chatroom = current_user.chatrooms.includes(:users).find(params[:id])
   end
 end
