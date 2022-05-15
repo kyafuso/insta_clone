5255b30d07a512f19dd0993cb492a5ac476621f3
kyafuso <kazuya.yafuso.0825@gmail.com>
2022-05-15 12:55:08 +0900

    [fix]不正なパラメータを許容してしまっていた

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomsController < ApplicationController
   before_action :require_login, only: %i[index show]
+  before_action :require_user_ids, only: %i[create]
 
   def index
     @chatrooms = current_user.chatrooms.includes(:users, :messages).page(params[:page]).order(created_at: :desc)
@@ -15,5 +16,11 @@ class ChatroomsController < ApplicationController
 
   def show
     @chatroom = current_user.chatrooms.includes(:users).find(params[:id])
+  end
+
+  private
+
+  def require_user_ids
+    redirect_back(fallback_location: root_path, danger: 'パラメータが不正です') if params.dig(:chatroom, :user_ids).reject(&:blank?).blank?
   end
 end
